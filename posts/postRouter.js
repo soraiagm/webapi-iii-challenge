64385ee360f9191e7651ea0048fe494432e85f9a
Soraia <soraia@Soraias-iMac.local>
2019-08-18 17:15:05 -0400

    created postRouter and userRouter folder and created some custom middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,106 @@
-const express = 'express';
+const express = require('express');
+
+const posts = require('./postDb.js');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+    console.log('We are in the posts router!');
+    next();
+});
+
 router.get('/', (req, res) => {
-
+    posts.get(req.query)
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: "The posts information could not be retrieved"
+            });
+        })
 });
 
-router.get('/:id', (req, res) => {
-
+// api/posts/:id
+router.get('/:id', validatePostId, (req, res) => {
+    posts.getById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(hub);
+            } else {
+                res.status(404).json({
+                    message: 'Post not found'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error retrieving post'
+            });
+       });
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, (req, res) => {
+    posts.remove(req.params.id)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({
+                    message: 'The post has been deleted'
+                });
+            } else {
+                res.status(404).json({
+                    message: 'The post could not be found'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error removing the post'
+            });
+        });
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, (req, res) => {
+    posts.update(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({
+                    message: 'The post could not be found'
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Error updating the post',
+            });
+        });
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
+    const { id } = req.params;
 
+    posts.getById(id)
+        .then(post => {
+            if (post) {
+                req.post = post;
+                next();
+            } else {
+                res.status(404).json({
+                    message: 'No post with given id'
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                message: 'Error processing request'
+            });
+        });
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
-const express = 'express';
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const postRouter = require('./posts/postRouter.js');
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
 
+const bodyParser = express.json();
+server.use(bodyParser);
+
+server.use(helmet());
+server.use(morgan('dev'));
+
+server.use(logger);
+server.use(addName);
+
+server.use('/api/posts', postRouter);
+server.use('./api/users', userRouter);
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  const nameInsert = (req.name) ? ` ${req.name}` : '';
+  res.send(`
+  <h2>Let's write some middleware!</h2>
+  <p>Welcome${nameInsert} to the Lambda Users API</p>
+  `);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`${req.method} Request`);
+  next();
 };
+
+function addName(req, res, next) {
+  req.name = "Cassandra";
+  next();
+}
 
 module.exports = server;
